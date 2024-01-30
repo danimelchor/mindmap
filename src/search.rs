859cf3fd291d3c1cb1e169b90570466067713ed6
danimelchor <dmelchor@pm.me>
2024-01-30 01:29:40 -0500

    search with cos dist work

--- src/search.rs
+++ src/search.rs
@@ -1,56 +1,49 @@
-use std::path::PathBuf;
-
-use kd_tree::KdPoint;
+use acap::{cos::angular_distance, Distance};
 
 use crate::{config::MindmapConfig, database::EmbeddedSentence, embeddings::Model};
 
-impl KdPoint for EmbeddedSentence {
-    type Scalar = f32;
-    type Dim = typenum::U2; // 2 dimensional tree.
-    fn at(&self, k: usize) -> f32 {
-        self.embedding[k]
-    }
-}
-
 pub struct Tree {
-    kd_tree: Box<kd_tree::KdTree<EmbeddedSentence>>,
     config: MindmapConfig,
+    corpus: Vec<EmbeddedSentence>,
 }
 
 impl Tree {
     pub fn new(corpus: Vec<EmbeddedSentence>, config: MindmapConfig) -> Self {
-        let kd_tree = kd_tree::KdTree::build_by(corpus, |item1, item2, k| {
-            item1.embedding[k].partial_cmp(&item2.embedding[k]).unwrap()
-        });
-        Tree {
-            kd_tree: Box::new(kd_tree),
-            config,
-        }
+        Tree { config, corpus }
     }
 
     pub fn search(self, query: &String) {
         let model = Model::new(&self.config.model).unwrap();
         let emb = model.encode(query).expect("Failed to encode query");
-        let rich_topic = EmbeddedSentence {
-            path: PathBuf::from(""),
-            start_line_no: 0,
-            end_line_no: 0,
-            embedding: emb,
-        };
-        let nearests = self.kd_tree.nearests(&rich_topic, 10);
-        for nearest in nearests {
+
+        let corpus_emb = self
+            .corpus
+            .iter()
+            .map(|x| {
+                let dist = angular_distance(&emb, &x.embedding);
+                (x, dist)
+            })
+            .collect::<Vec<_>>();
+
+        // Sort
+        let mut nearests = corpus_emb;
+        nearests.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+        // Print
+        println!("Nearest sentences:");
+        for (x, dist) in nearests.iter().take(10) {
             println!(
                 "{}:{}:{} ({})",
-                nearest.item.path.to_str().unwrap(),
-                nearest.item.start_line_no,
-                nearest.item.end_line_no,
-                nearest.squared_distance
+                x.path.to_str().unwrap(),
+                x.start_line_no,
+                x.end_line_no,
+                dist.value()
             );
 
-            let content = std::fs::read_to_string(&nearest.item.path).unwrap();
+            let content = std::fs::read_to_string(&x.path).unwrap();
             let lines: Vec<&str> = content.split("\n").collect();
-            let start = nearest.item.start_line_no - 1;
-            let end = nearest.item.end_line_no;
+            let start = x.start_line_no - 1;
+            let end = x.end_line_no;
             for i in start..end {
                 println!("{:?}", lines[i as usize]);
             }
